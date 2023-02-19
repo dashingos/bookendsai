f5f9995b49284f3fa8099169fac4850e5e1e3d85
Andrew Heller <aheller9@gmail.com>
2023-02-19 12:19:30 -0600

    bookends initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+	import '../app.css';
+</script>
+
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+	<link rel="preconnect" href="https://cdn.fontshare.com" crossOrigin="anonymous" />
+	<link
+		rel="stylesheet"
+		href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap"
+	/>
+	<link
+		rel="stylesheet"
+		href="https://api.fontshare.com/v2/css?f[]=cabinet-grotesk@800,500,700&display=swap"
+	/>
+</svelte:head>
+
+<div class="scroll-smooth bg-white antialiased">
+	<slot />
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<div class="max-w-2xl m-auto mt-32">
+	<h1 class="font-display text-5xl font-extrabold text-slate-900 md:text-6xl text-center">
+		◫ Bookends AI
+	</h1>
+	<p class="my-4 text-3xl text-slate-600 text-center">Search for a book or author</p>
+
+	<div
+		class="mt-5 flex rounded-3xl bg-white py-2.5 pr-2.5 shadow-xl shadow-blue-900/5 focus-within:ring-2 focus-within:ring-blue-900 border border-slate-100"
+	>
+		<input
+			required=""
+			placeholder="Atomic Habits by James Clear"
+			aria-label="Search"
+			class="-my-2.5 flex-auto bg-transparent pl-6 pr-2.5 text-base text-slate-900 placeholder:text-slate-400 focus:outline-none border-0"
+		/><button
+			class="inline-flex justify-center rounded-2xl bg-blue-600 py-4 px-6 text-base font-semibold text-white hover:bg-blue-500 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:text-white/70 "
+			type="submit"
+			><span class="sr-only sm:not-sr-only">Search</span><span class="sm:hidden"
+				><svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="w-6 h-6"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+					/>
+				</svg>
+			</span></button
+		>
+	</div>
+</div>
