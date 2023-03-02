c31c6e7ee17efb52e996f3040254a1e06dacf1de
Andrew Heller <aheller9@gmail.com>
2023-03-02 09:34:25 -0600

    wrap

--- src/routes/book/(dynamic)/[id]/+page.svelte
+++ src/routes/book/(dynamic)/[id]/+page.svelte
@@ -55,11 +55,11 @@
 
 {#if loaded}
 	{#if volumeInfo}
-		<div class="flex items-end mb-8">
+		<div class="sm:flex items-end mb-8">
 			<img
 				src={volumeInfo.imageLinks.thumbnail}
 				alt="{volumeInfo.title} Cover"
-				class="object-contain border border-slate-100 rounded"
+				class="object-contain border border-slate-100 rounded m-4"
 			/>
 			<div class="ml-4">
 				<h1 class="font-bold text-3xl mb-1 font-display">{volumeInfo.title}</h1>
@@ -74,13 +74,15 @@
 							href={links[title].aff}
 							target="_blank"
 							rel="noreferrer"
-							class="text-sm text-slate-500 bg-slate-100 px-4 py-2  rounded">Buy Book</a
+							class="text-sm text-slate-500 bg-slate-100 px-4 py-2 rounded whitespace-nowrap my-1 mr-2 inline-block"
+							>Buy Book</a
 						>
 						<a
 							href={links[title].audible}
 							target="_blank"
 							rel="noreferrer"
-							class="text-sm text-slate-500 bg-slate-100 px-4 py-2 rounded">Buy Audiobook</a
+							class="text-sm text-slate-500 bg-slate-100 px-4 py-2 rounded whitespace-nowrap my-1 inline-block"
+							>Buy Audiobook</a
 						>
 					</p>
 				{/if}
